a0128e9b7aea3ad0713260972e44883ee5aa64cf
Graciliano M. Passos <gracilianomp@gmail.com>
2018-07-23 00:23:30 -0300

    Optimized implementation of StackType.getStack(...), without switch.

--- src/main/java/org/gracilianomp/function/StackType.java
+++ src/main/java/org/gracilianomp/function/StackType.java
@@ -1,7 +1,46 @@
 package org.gracilianomp.function;
 
+import org.gracilianomp.arithmetic.MathValue;
+
 public enum StackType {
-    INPUT,
-    RUNTIME,
-    GLOBAL,
+    INPUT(new GetterInput()),
+    RUNTIME(new GetterRuntime()),
+    GLOBAL(new GetterGlobal())
+    ;
+
+    private final Getter getter ;
+
+    StackType(Getter getter) {
+        this.getter = getter;
+    }
+
+    final public MathStack getStack(MathFunction function) {
+        return getter.getStack(function) ;
+    }
+
+    abstract static class Getter {
+        abstract public <V extends MathValue> MathStack<V> getStack(MathFunction<V> function) ;
+    }
+
+    final static class GetterInput extends Getter {
+        @Override
+        public <V extends MathValue> MathStack<V> getStack(MathFunction<V> function) {
+            return function.getInputStack();
+        }
+    }
+
+    final static class GetterRuntime extends Getter {
+        @Override
+        public <V extends MathValue> MathStack<V> getStack(MathFunction<V> function) {
+            return function.getRuntimeStack() ;
+        }
+    }
+
+    final static class GetterGlobal extends Getter {
+        @Override
+        public <V extends MathValue> MathStack<V> getStack(MathFunction<V> function) {
+            return function.getGlobalStack();
+        }
+    }
+
 }
